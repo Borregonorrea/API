92551e9d159fdc334eefdb7cd3eb1943bc378eb2
Borregonorrea <samuel.orregoc@upb.edu.co>
2024-10-25 18:57:52 -0500

    First commit

---
+++ API_Reservas/Controllers/ReservasController.cs
@@ -0,0 +1,64 @@
+﻿using API_Reservas.Modelo;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API_Reservas.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ReservasController : Controller
+    {
+
+        private static List<Reserva> reservas = new List<Reserva>
+        {
+            new Reserva(1, "Juan Pérez", DateTime.Now.AddDays(1), 4, "Confirmada"),
+            new Reserva(2, "Ana Lopez", DateTime.Now.AddDays(2), 2, "Pendiente"),
+            new Reserva(3, "Carlos Diaz", DateTime.Now.AddDays(3), 3, "Cancelada"),
+            new Reserva(4, "Maria Gonzales", DateTime.Now.AddDays(4), 5, "Confirmada")
+
+        };
+
+        [HttpGet]
+        public ActionResult<IEnumerable<Reserva>> GetReservas()
+        {
+            return Ok(reservas);
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<Reserva> GetReserva(int id)
+        {
+            var reserva = reservas.FirstOrDefault(r => r.ID == id);
+
+            if (reserva == null)
+            {
+                return NotFound($"La reserva con ID {id} no fue encontrada.");
+            }
+
+            return Ok(reserva);
+        }
+
+        [HttpPost]
+        public ActionResult<Reserva> CrearReserva([FromBody] Reserva nuevaReserva)
+        {
+            // Asigna un ID automáticamente sumando 1 al último ID
+            int nuevoId = reservas.Max(r => r.ID) + 1;
+            nuevaReserva.ID = nuevoId;
+
+            // Validar la reserva
+            if (!nuevaReserva.ValidarEstado())
+            {
+                return BadRequest("El estado de la reserva no es válido.");
+            }
+            if (!nuevaReserva.ValidarNumeroPersonas())
+            {
+                return BadRequest("El número de personas debe estar entre 1 y 20.");
+            }
+
+            reservas.Add(nuevaReserva);
+
+            return CreatedAtAction(nameof(GetReserva), new { id = nuevaReserva.ID }, nuevaReserva);
+        }
+
+
+
+    }
+}
---
+++ API_Reservas/Modelo/Reserva.cs
@@ -0,0 +1,34 @@
+﻿namespace API_Reservas.Modelo
+{
+    public class Reserva
+    {
+        public int ID { get; set; }
+        public string cliente { get; set; }
+        public DateTime fecha_reserva { get; set; }
+        public int numero_personas { get; set; }
+        public string estado { get; set; } // Confirmada, Pendiente, Cancelada
+
+        public Reserva(int iD, string cliente, DateTime fecha_reserva, int numero_personas, string estado)
+        {
+            ID = iD;
+            this.cliente = cliente;
+            this.fecha_reserva = fecha_reserva;
+            this.numero_personas = numero_personas;
+            this.estado = estado;
+        }
+
+        public bool ValidarEstado()
+        {
+            string[] estados_validos = { "Confirmada", "Pendiente", "Cancelada" };
+            return Array.Exists(estados_validos, estado => estado == this.estado);
+
+        }
+
+        public bool ValidarNumeroPersonas()
+        {
+            return numero_personas > 0 && numero_personas <= 20;
+        }
+
+
+    }
+}
